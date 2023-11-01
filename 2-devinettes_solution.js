7de17b8ca544db65e9e9f37db46c0de331ff52af
vivienbur <149022066+vivienbur@users.noreply.github.com>
2023-11-01 16:03:07 +0000

    update

--- 2-devinettes_solution.js
+++ 2-devinettes_solution.js
@@ -1,17 +1,20 @@
 const prompt = require('prompt-sync')();
 
 let nombreAleatoire = Math.floor(Math.random() * 100) + 1;
+
 let nombreEssais;
 
-for (nombreEssais = 0; nombreEssais < 10; nombreEssais++) {
+for (nombreEssais = 1; nombreEssais < 11; nombreEssais++) {
+
   const nombreSaisi = prompt("Devine un nombre: ");
 
   if (nombreSaisi < nombreAleatoire) {
     console.log("Plus grand");
   } else if (nombreSaisi > nombreAleatoire) {
-      console.log("Plus petit");
+    console.log("Plus petit");
   } else {
-      console.log("GAGNÉ!!! en " + (nombreEssais + 1) + " essais");
+    console.log("GAGNÉ!!");
+    console.log("En " + nombreEssais + " essais");
     break;
   }
 }
